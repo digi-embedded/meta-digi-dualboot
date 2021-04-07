1ca50e7e57b3c438a7f07f9fce437ea2bfe6f7ab
Mike Engel <Mike.Engel@digi.com>
2021-04-07 09:01:56 +0200

    dualboot: add firmware update scripts
    This commit adds scripts to start the firmware update
    and a systemd service to check update success.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>
    
    https://jira.digi.com/browse/DEL-7486

---
+++ recipes-digi/dualboot/dualboot.bb
@@ -0,0 +1,51 @@
+# Copyright (C) 2021 Digi International Inc.
+
+SUMMARY = "Digi Embedded Yocto Dual boot support"
+SECTION = "base"
+LICENSE = "GPL-2.0"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=801f80980d171dd6425610833a22dbe6"
+
+SRC_URI = " \
+    file://dualboot-init \
+    file://firmware-update-dual.sh \
+    file://firmware-update-check.service \
+"
+
+S = "${WORKDIR}"
+
+inherit systemd
+
+do_install() {
+	install -d ${D}${sysconfdir}/init.d/
+	install -m 0755 ${WORKDIR}/dualboot-init ${D}${sysconfdir}/dualboot-init
+	ln -sf /etc/dualboot-init ${D}${sysconfdir}/init.d/dualboot-init
+
+	install -d ${D}${bindir}
+	install -m 0755 ${WORKDIR}/firmware-update-dual.sh ${D}${bindir}
+
+	install -d ${D}${systemd_unitdir}/system/
+	install -m 0644 ${WORKDIR}/firmware-update-check.service ${D}${systemd_unitdir}/system/
+}
+
+PACKAGES =+ "${PN}-init"
+FILES_${PN}-init = " \
+    ${sysconfdir}/dualboot-init \
+    ${sysconfdir}/init.d/dualboot-init \
+    ${bindir}/firmware-update-dual.sh \
+    ${systemd_unitdir}/system/firmware-update-check.service \
+"
+
+INITSCRIPT_PACKAGES += "${PN}-init"
+INITSCRIPT_NAME_${PN}-init = "dualboot-init"
+INITSCRIPT_PARAMS_${PN}-init = "start 19 2 3 4 5 . stop 21 0 1 6 ."
+
+SYSTEMD_PACKAGES = "${PN}-init"
+SYSTEMD_SERVICE_${PN}-init = "firmware-update-check.service"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+# Add swupdate into the rootfs for dual boot support
+RDEPENDS_${PN}-init = " \
+    swupdate \
+"
+
---
+++ recipes-digi/dualboot/dualboot/firmware-update-dual.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+#===============================================================================
+#
+#  update-firmware-dual.sh
+#
+#  Copyright (C) 2021 by Digi International Inc.
+#  All rights reserved.
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License version 2 as published by
+#  the Free Software Foundation.
+#
+#
+#  !Description: Dual boot firmware update script
+#
+#===============================================================================
+
+# Check update file parameter.
+UPDATE_FILE="${1}"
+if [ -z "${UPDATE_FILE}" ]; then
+	echo "[ERROR] Update file not specified"
+	exit
+fi
+
+PARTTABLE="/proc/mtd"
+MTDINDEX="$(sed -ne "s/\(^mtd[0-9]\+\):.*\<environment\>.*/\1/g;T;p" ${PARTTABLE} 2>/dev/null)"
+
+if [ -z "${MTDINDEX}" ]; then
+	# Get Boot partition device and index.
+	BOOT_PART="$(fw_printenv -n mmcpart 2>/dev/null)"
+	BOOT_DEV="$(fw_printenv -n mmcbootdev 2>/dev/null)"
+
+	CURRENT_PART="$(ls -l /dev/disk/by-partlabel/ | grep -i mmcblk${BOOT_DEV}p${BOOT_PART} | awk '{print $9}')"
+
+	# Get current partition information so we can
+	# determine where to flash the images.
+	if [ "${CURRENT_PART}" = "linux_a" ]; then
+		echo "Current system is A; Updating system on B"
+		KERNELBOOT="linux_b"
+		ROOTFS="rootfs_b"
+		IMAGE_SET="mmc,secondary"
+	else
+		echo "Current system is B; Updating system on A"
+		KERNELBOOT="linux_a"
+		ROOTFS="rootfs_a"
+		IMAGE_SET="mmc,primary"
+	fi
+
+	# get boot partition index
+	MMC_PART="$(ls -l /dev/disk/by-partlabel/ | grep -i ${KERNELBOOT} | awk '{print $11}' | sed -e 's/[../mmcblkp]//g' -e 's/^.//')"
+
+	# search rootfs UUID
+	MMCROOT_PART="$(ls -l /dev/disk/by-partlabel/ | grep -i ${ROOTFS} | awk '{print $11}' | sed -e 's/[../mmcblkp]//g' -e 's/^.//')"
+	PART_UUID="$(ls -l /dev/disk/by-partuuid/ | grep -i mmcblk${BOOT_DEV}p${MMCROOT_PART} | awk '{print $9}')"
+
+	echo ""
+	echo "Updating '${IMAGE_SET}' image set from '${UPDATE_FILE}'..."
+	echo ""
+
+	# Execute the update.
+	swupdate -v -i "${UPDATE_FILE}" -e "${IMAGE_SET}"
+	if [ "$?" = "0" ]; then
+		fw_setenv mmcroot PARTUUID=${PART_UUID}
+		fw_setenv mmcpart ${MMC_PART}
+		fw_setenv active_system ${KERNELBOOT}
+		echo "Firmware update finished; Rebooting system."
+		reboot -f
+	else
+		echo "[ERROR] $? There was an error performing the update"
+	fi
+else
+	# Get Boot partition device and index.
+	MTD_BOOT_PART="$(fw_printenv -n mtdbootpart 2>/dev/null)"
+
+	# Get current partition information so we can
+	# determine where to flash the images.
+	if [ "${MTD_BOOT_PART}" = "linux_a" ]; then
+		echo "Current system is A; Updating system on B"
+		KERNELBOOT="linux_b"
+		ROOTFS="rootfs_b"
+		IMAGE_SET="mtd,secondary"
+	else
+		echo "Current system is B; Updating system on A"
+		KERNELBOOT="linux_a"
+		ROOTFS="rootfs_a"
+		IMAGE_SET="mtd,primary"
+	fi
+
+	# get boot partition index
+	LINUX_INDEX="$(cat /proc/mtd | grep -i ${KERNELBOOT} | awk '{print $1}' | sed -e 's/[mtd]//g' -e 's/://')"
+
+	# get rootfs index
+	ROOTFS_INDEX="$(cat /proc/mtd | grep -i ${ROOTFS} | awk '{print $1}' | sed -e 's/[mtd]//g' -e 's/://')"
+
+	echo ""
+	echo "Updating '${IMAGE_SET}' image set from '${UPDATE_FILE}'..."
+	echo ""
+
+	# Execute the update.
+	swupdate -v -i "${UPDATE_FILE}" -e "${IMAGE_SET}"
+	if [ "$?" = "0" ]; then
+		fw_setenv mtdlinuxindex ${LINUX_INDEX}
+		fw_setenv mtdrootfsindex ${ROTFS_INDEX}
+		fw_setenv mtdbootpart ${KERNELBOOT}
+		fw_setenv active_system ${KERNELBOOT}
+		echo "Firmware update finished; Rebooting system."
+		reboot -f
+	else
+		echo "[ERROR] $? There was an error performing the update"
+	fi
+fi
